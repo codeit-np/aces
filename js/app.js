12f76fd5fab555958915b26f19e011af9e1b11b4
Sajal Shrestha <codeit.np@gmail.com>
2024-06-28 07:58:09 +0545

    task2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-//Create div element with styles
 let div = document.createElement("div");
-div.style.cssText = "border:2px solid black;width:300px";
+div.style.cssText = "border:2px solid red";
 
-// Create H1 element with content
 let h1 = document.createElement("h1");
+let img = document.createElement("img");
+
 h1.innerHTML = "Hello World";
+img.src = "https://img.freepik.com/free-vector/hand-coding-concept-illustration_114360-8113.jpg?t=st=1719540696~exp=1719544296~hmac=93a4babbc52e3a31d0e7e7eeb3777d8e7f71ca71bea18bbdcc6dc6d5e08fb46f&w=1060";
 
-// Create p Element with content
-let p = document.createElement("p");
-p.innerHTML = "I love coding. do you?";
-
-//Add h1 and p to div
+div.appendChild(img);
 div.appendChild(h1);
-div.appendChild(p);
 
-//Add div to body
 document.body.appendChild(div);
